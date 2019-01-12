6cccc9a827b11ba64fbddeb02fe402232bb04e22
hari <hari@haris-MacBook-Pro.local>
2019-01-12 06:02:00 -0700

    checking for failure

--- src/index.test.js
+++ src/index.test.js
@@ -4,7 +4,7 @@ import {describe, it} from "eslint/lib/testers/event-generator-tester";
 
 describe('Our first Test', () => {
     it('true to be true', () => {
-        expect(true).to.equal(false);
+        expect(true).to.equal(2);
     } );
 
 });
