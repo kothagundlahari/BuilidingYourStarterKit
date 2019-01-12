55954232d1efb1edf682e663774cc5928840b2a1
hari <hari@haris-MacBook-Pro.local>
2019-01-12 05:56:27 -0700

    added the test suite

---
+++ src/index.test.js
@@ -0,0 +1,25 @@
+import {expect} from 'chai';
+import {describe, it} from "eslint/lib/testers/event-generator-tester";
+
+
+describe('Our first Test', () => {
+    it('true to be true', () => {
+        expect(true).to.equal(true);
+    } );
+
+});
+
+describe('index.html', () => {
+    it('It should print hello world', (done) => {
+        const index = fs.readFileSync('./src/index.html', "utf-8");
+        jsdom.env(index, (window) => {
+            const h1 = window.document.getElementById('hello')[0];
+            expect(h1.innerHTML).to.equal('Hello');
+            done();
+            window.close();
+
+        })
+    })
+
+});
+
